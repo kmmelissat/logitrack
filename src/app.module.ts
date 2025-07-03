a7ea553ed17bea704bcbacf8b1cdca9101e788fa
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-07-02 18:55:49 -0600

    ➕ Add MapsModule to app.module.ts imports for enhanced mapping functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { MaintenanceModule } from './maintenance/maintenance.module';
+import { MapsModule } from './maps/maps.module';
 import { GpsEventModule } from './gps-event/gps-event.module';
 import { VehicleCheckinModule } from './vehicle-checkin/vehicle-checkin.module';
 import { RoutePointModule } from './route-point/route-point.module';
@@ -14,7 +15,7 @@ import { VehicleModule } from './vehicle/vehicle.module';
 import { MaintenanceModule } from './maintenance/maintenance.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, VehiclesModule, MaintenanceModule, UserModule, VehicleModule, ScheduledRouteModule, RoutePointModule, VehicleCheckinModule, GpsEventModule],
+  imports: [UsersModule, AuthModule, VehiclesModule, MaintenanceModule, UserModule, VehicleModule, ScheduledRouteModule, RoutePointModule, VehicleCheckinModule, GpsEventModule, MapsModule],
   controllers: [AppController],
   providers: [AppService],
 })
